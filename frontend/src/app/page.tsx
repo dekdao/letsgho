c00f3c34aaae58b818dc2a5e83d63bed10e50120
mrmuffinnxz <71134901+mrmuffinnxz@users.noreply.github.com>
2024-01-15 21:27:47 +0700

    feat: setup project

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import { TypographySmall } from "@/components/ui/typography";
+
+export default function Home() {
+  return (
+    <div>
+      <TypographySmall>Home</TypographySmall>
+      <Button>Click me</Button>
+    </div>
+  );
+}
